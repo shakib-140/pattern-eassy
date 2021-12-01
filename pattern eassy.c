7854423881315e32356d6b5062d9436e3abc00ac
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-01 23:36:26 +0600

    Add files via upload

---
+++ pattern eassy.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+printf("enter the value of n=");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    printf("%d\t",i);
+
+
+}
+
+
+    getch();
+}
